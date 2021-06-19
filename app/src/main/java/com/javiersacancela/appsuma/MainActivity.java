e038a4dc5d5ff86e12fd5896518defe4c347f425
Javier Sacancela <javiersacancela@gmail.com>
2021-06-18 22:56:31 -0500

    calculadora v01

---
+++ app/src/main/java/com/javiersacancela/appsuma/MainActivity.java
@@ -0,0 +1,40 @@
+package com.javiersacancela.appsuma;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    private EditText num1;
+    private EditText num2;
+    private TextView result;
+    private Button btnSuma;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        result = findViewById(R.id.result);
+        num1 = findViewById(R.id.num1);
+        num2 = findViewById(R.id.num2);
+        btnSuma = findViewById(R.id.btnSuma);
+
+    }
+
+       //Metodo para ejecutar el boton
+        public void btnSuma(View view){
+            String valor1 = num1.getText().toString();
+            String valor2 = num2.getText().toString();
+            int n1 = Integer.parseInt(valor1);
+            int n2 = Integer.parseInt(valor2);
+            int add = n1 + n2;
+            String res = String.valueOf(add);
+            result.setText(res);
+  }
+}
